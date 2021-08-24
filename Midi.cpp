58e921897c56c50868da940c3b5486f99c78ca8b
ArcAudio <67969830+ArcAudio@users.noreply.github.com>
2021-08-24 18:10:09 +0100

    Add files via upload

---
+++ Midi.cpp
@@ -0,0 +1,216 @@
+#include "daisy_patch.h"
+#include "daisysp.h"
+#include <string>
+
+using namespace daisy;
+using namespace daisysp;
+
+DaisyPatch hw;
+#define NUM_VOICES 1
+
+float trig;
+
+class Voice
+{
+  public:
+    Voice() {}
+    ~Voice() {}
+    void Init(float samplerate)
+    {
+        active_ = false;
+        osc_.Init(samplerate);
+        //osc_.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW);
+        env_.Init(samplerate);
+        env_.SetSustainLevel(0.5f);
+        env_.SetTime(ADSR_SEG_ATTACK, 0.005f);
+        env_.SetTime(ADSR_SEG_DECAY, 0.005f);
+        env_.SetTime(ADSR_SEG_RELEASE, 0.2f);
+    }
+
+    float Process()
+    {
+        if(active_)
+        {
+            float sig, amp;
+            amp = env_.Process(env_gate_);
+            if(!env_.IsRunning())
+                active_ = false;
+            //sig = osc_.Process();
+
+            return sig * (velocity_ / 127.f) * amp;
+        }
+        return 0.f;
+    }
+
+    void OnNoteOn(float note, float velocity)
+    {
+        note_     = note;
+        velocity_ = velocity;
+        //osc_.SetFreq(mtof(note_));
+        
+        active_   = true;
+        env_gate_ = true;
+        //Process(trig, note_);
+    }
+
+    void OnNoteOff() { env_gate_ = false; }
+
+    inline bool  IsActive() const { return active_; }
+    inline float GetNote() const { return note_; }
+
+  private:
+    //Oscillator osc_;
+    PolyPluck<NUM_VOICES> osc_;
+    //Svf        filt_;
+    Adsr       env_;
+    float      note_, velocity_;
+    bool       active_;
+    bool       env_gate_;
+};
+
+template <size_t max_voices>
+class VoiceManager
+{
+  public:
+    VoiceManager() {}
+    ~VoiceManager() {}
+
+    void Init(float samplerate)
+    {
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            voices[i].Init(samplerate);
+        }
+    }
+
+    float Process()
+    {
+        float sum;
+        sum = 0.f;
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            sum += voices[i].Process();
+        }
+        return sum;
+    }
+
+    void OnNoteOn(float notenumber, float velocity)
+    {
+        Voice *v = FindFreeVoice();
+        if(v == NULL)
+            return;
+        v->OnNoteOn(notenumber, velocity);
+    }
+
+    void OnNoteOff(float notenumber, float velocity)
+    {
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            Voice *v = &voices[i];
+            if(v->IsActive() && v->GetNote() == notenumber)
+            {
+                v->OnNoteOff();
+            }
+        }
+    }
+
+    void FreeAllVoices()
+    {
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            voices[i].OnNoteOff();
+        }
+    }
+
+  private:
+    Voice  voices[max_voices];
+    Voice *FindFreeVoice()
+    {
+        Voice *v = NULL;
+        for(size_t i = 0; i < max_voices; i++)
+        {
+            if(!voices[i].IsActive())
+            {
+                v = &voices[i];
+                break;
+            }
+        }
+        return v;
+    }
+};
+
+static VoiceManager<24> voice_handler;
+
+void AudioCallback(AudioHandle::InputBuffer  in,
+                   AudioHandle::OutputBuffer out,
+                   size_t                                size)
+{
+    float sum = 0.f;
+    hw.ProcessDigitalControls();
+    hw.ProcessAnalogControls();
+    float *out_left, *out_right;
+    out_left  = out[0];
+    out_right = out[1];
+
+    //voice_handler.SetCutoff(250.f + hw.GetKnobValue(hw.KNOB_1) * 8000.f);
+    //voice_handler.SetSomethingHere
+    //for(size_t i = 0; i < size; i += 2)??
+    for(size_t i = 0; i < size; i ++)
+    {
+        sum        = voice_handler.Process() * 0.5f;
+        out_left   = sum;
+        out_right  = sum;
+    }
+}
+
+// Typical Switch case for Message Type.
+void HandleMidiMessage(MidiEvent m)
+{
+    switch(m.type)
+    {
+        case NoteOn:
+        {
+            NoteOnEvent p = m.AsNoteOn();
+            // Note Off can come in as Note On w/ 0 Velocity
+            if(p.velocity == 0.f)
+            {
+                voice_handler.OnNoteOff(p.note, p.velocity);
+            }
+            else
+            {
+                voice_handler.OnNoteOn(p.note, p.velocity);
+            }
+        }
+        break;
+        case NoteOff:
+        {
+            NoteOnEvent p = m.AsNoteOn();
+            voice_handler.OnNoteOff(p.note, p.velocity);
+        }
+        break;
+        default: break;
+    }
+}
+
+// Main -- Init, and Midi Handling
+int main(void)
+{
+    // Init
+    float samplerate;
+    hw.Init();
+    samplerate = hw.AudioSampleRate();
+    voice_handler.Init(samplerate);
+    // Start stuff.
+    hw.midi.StartReceive();
+    hw.StartAdc();
+    hw.StartAudio(AudioCallback);
+    for(;;)
+    {
+        hw.midi.Listen();
+        // Handle MIDI Events
+        while(hw.midi.HasEvents())
+        {
+            HandleMidiMessage(hw.midi.PopEvent());
+        }
+    }
+}
